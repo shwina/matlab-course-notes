835dcdd5728746b54b5d011a6ff31d855d1083a4
Ashwin Srinath <ashwinsrnth@gmail.com>
2015-02-22 11:49:08 -0500

    update excel notes

--- excel_io.m
+++ excel_io.m
@@ -11,7 +11,7 @@
 
 %% Excel Input and Output
 
-%% 
+%% Reading Data from Excel Files
 % Here's the data in the file |RaceResults.xlsx|:
 
 %%
@@ -32,6 +32,7 @@
 % |xlsread| returns two values, |num| and |txt|.
 %
 % 1. |num| is a *matrix* containing all the numeric data
+%
 % 2. |txt| is a *cell array* containing all the text data
 
 
@@ -95,5 +96,62 @@ player_names = txt(3:end, 2)
 
 %%
 % Don't confuse this with using curly brackets |{}| to access
-% values from a cell array, which _doesn't_ put the values
+% values from a cell array, which _doesn't_ put values
 % in a new cell array.
+
+%% Writing Data to Excel Files
+%
+% Let's say we have some new data that we'd
+% like to write into |RaceResults.xlsx|:
+
+new_name = {'Koopa'};
+new_lap_times = [18.1, 18.9, 22.4, 23.6, 17.1];
+
+%%
+% We can do this as follows:
+
+xlswrite('RaceResults.xlsx', 5, 1, 'A7');
+xlswrite('RaceResults.xlsx', new_name, 1, 'B7');
+xlswrite('RaceResults.xlsx', new_lap_times, 1, 'C7');
+
+%%
+% Now have a look at |RaceResults.xlsx|:
+
+%%
+% 
+% <html>
+% <img src="../mariokart_data_new.png">
+% </html>
+
+%%
+% The line
+
+xlswrite('RaceResults.xlsx', 5, 1, 'A7');
+
+%%
+% tells MATLAB we want to write the number |5| to
+% |RaceResults.xlsx|, in Sheet |1|, Cell |A7|.
+
+%%
+% Similarly, the line
+
+xlswrite('RaceResults.xlsx', new_name, 1, 'B7');
+
+%%
+% tells MATLAB we want to write the cell array
+% |new_name| to |RaceResults.xlsx|, in Sheet |1|,
+% Cell |B7|. Remember, when you're writing 
+% a string, or a collection of strings to
+% an excel file, you need to put them in a cell
+% array first. That's why |new_name| is
+% |{'Koopa'}|, and not |'Koopa'|.
+
+%%
+% And finally, the line
+
+xlswrite('RaceResults.xlsx', new_lap_times, 1, 'C7');
+
+%%
+% tells MATLAB we want to write the
+% vector |new_lap_times| to |RaceResults.xlsx|,
+% in Sheet |1| and beginning at Cell |C7|.
